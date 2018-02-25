d98b1f5a97bf5f094f45f305e0f73be0a356cde8
Ronak Shah <ronakshah2013@u.northwestern.edu>
2018-02-25 15:37:02 -0600

    Initial commit

---
+++ public/traintime-logic.js
@@ -0,0 +1,77 @@
+
+var config = {
+  apiKey: "AIzaSyAifPs7Mdf55PQ2X22yhUt-ghqGKu3R38c",
+  authDomain: "traintracker-88545.firebaseapp.com",
+  databaseURL: "https://traintracker-88545.firebaseio.com",
+  projectId: "traintracker-88545",
+  storageBucket: "",
+  messagingSenderId: "286860241111"
+};
+firebase.initializeApp(config);
+
+var trainData = firebase.database();
+
+$("#add-train-btn").on("click", function() {
+
+  var trainName = $("#train-name-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var firstTrain = $("#first-train-input").val().trim();
+  var frequency = $("#frequency-input").val().trim();
+
+  var newTrain = {
+
+    name: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
+  };
+
+  trainData.ref().push(newTrain);
+
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstTrain);
+  console.log(newTrain.frequency);
+
+  alert("Train successfully added");
+
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+
+  return false;
+});
+
+trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+
+      var tName = childSnapshot.val().name;
+      var tDestination = childSnapshot.val().destination;
+      var tFrequency = childSnapshot.val().frequency;
+      var tFirstTrain = childSnapshot.val().firstTrain;
+
+      var timeArr = tFirstTrain.split(":");
+      var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+      var maxMoment = moment.max(moment(), trainTime);
+      var tMinutes;
+      var tArrival;
+      
+      if (maxMoment === trainTime) {
+        tArrival = trainTime.format("hh:mm A");
+        tMinutes = trainTime.diff(moment(), "minutes");
+      } else {
+
+
+        var differenceTimes = moment().diff(trainTime, "minutes");
+        var tRemainder = differenceTimes % tFrequency;
+        tMinutes = tFrequency - tRemainder;
+        tArrival = moment().add(tMinutes, "m").format("hh:mm A");
+      }
+        console.log("tMinutes:", tMinutes);
+        console.log("tArrival:", tArrival);
+
+        $("#train-table > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" +
+          tFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
+      });
+
